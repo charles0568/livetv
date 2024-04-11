157e6a8b175b0b2d204290b5329bf799a77ac7f4
snowie2000 <mactype.feedback@gmail.com>
2024-04-11 10:15:08 +0800

    implemented a new parser that caches the http location redirection info and use that url as the final playlist

---
+++ plugin/follow.go
@@ -0,0 +1,39 @@
+// follow location
+package plugin
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/zjyl1994/livetv/model"
+)
+
+type URLM3U8Parser struct{}
+
+func (p *URLM3U8Parser) Parse(liveUrl string, lastInfo string) (*model.LiveInfo, error) {
+	client := http.Client{
+		Timeout: time.Second * 10,
+		CheckRedirect: func(req *http.Request, via []*http.Request) error {
+			return http.ErrUseLastResponse
+		},
+	}
+	req, err := http.NewRequest("GET", liveUrl, nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("User-Agent", DefaultUserAgent)
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	redir := resp.Header.Get("Location")
+	if redir == "" {
+		return nil, NoMatchFeed
+	}
+	directParser := &DirectM3U8Parser{}
+	return directParser.Parse(redir, lastInfo)
+}
+
+func init() {
+	registerPlugin("httpRedirect", &URLM3U8Parser{})
+}
